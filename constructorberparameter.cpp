4b8e229cdc184f5c4034e5e8f1f1d11fb9d700f4
radivaseptiana <radivaseptiana51@gmail.com>
2025-05-13 18:54:13 +0700

    mendeklarasikan constructor dengan parameter

--- constructorberparameter.cpp
+++ constructorberparameter.cpp
@@ -3,5 +3,13 @@
 using namespace std;
 
 class mahasiswa{
-    
+    public:
+    // Costructor dengan parameter
+    mahasiswa(int nim, string nama);
+};
+
+//Definisi constructor dengan parameter
+mahasiswa::mahasiswa(int nim, string nama){
+    cout << "Constructor Dengan Parameter Terpanggil" << endl;
+    cout
 }
