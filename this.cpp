8ab6d377eda5d4b86b6c8cf8c636bd30361a2a3e
radivaseptiana <radivaseptiana51@gmail.com>
2025-05-13 19:23:47 +0700

    membuat variabel judul

--- this.cpp
+++ this.cpp
@@ -2,5 +2,14 @@
 using namespace std;
 
 class buku{
-    
+   string judul;
+   public:
+        string setget(string jdl)
+        {
+            //nilai parameter 'judul' untuk member variabel 'judul'
+            this ->judul = jdl;
+            //return variabel judul
+            return this->judul;
+
+        }
 }
